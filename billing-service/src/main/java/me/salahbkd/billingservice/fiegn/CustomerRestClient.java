9df6943ade7d8db939696be6c49ddf7c7768759b
salahbkd <bkdsalah@gmail.com>
2021-11-29 21:36:09 +0100

    refactoring

--- billing-service/src/main/java/me/salahbkd/billingservice/fiegn/CustomerRestClient.java
+++ billing-service/src/main/java/me/salahbkd/billingservice/fiegn/CustomerRestClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "CUSTOMER-SERVICE")
+@FeignClient(name = "CUSTOMER-SERVICE") // OpenFeign library that allows to use declarative REST communication
 public interface CustomerRestClient {
     @GetMapping(path = "/customers/{id}")
     Customer getCustomerById(@PathVariable(name = "id") Long id);
--- billing-service/src/main/java/me/salahbkd/billingservice/fiegn/ProductItemRestClient.java
+++ billing-service/src/main/java/me/salahbkd/billingservice/fiegn/ProductItemRestClient.java
@@ -6,7 +6,7 @@ import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "INVENTORY-SERVICE")
+@FeignClient(name = "INVENTORY-SERVICE") // OpenFeign library that allows to use declarative REST communication
 public interface ProductItemRestClient {
     @GetMapping(path = "/products")
     PagedModel<Product> getPageProducts();
